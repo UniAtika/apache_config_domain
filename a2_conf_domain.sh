d92ae5469972257610cbefb7abceedef6ebc2ae1
UniAtika <56742694+UniAtika@users.noreply.github.com>
2019-10-25 10:52:51 +0200

    Update a2_conf_domain.sh

--- a2_conf_domain.sh
+++ a2_conf_domain.sh
@@ -20,7 +20,7 @@ export BLUE="\e[34m"
 clear;
 echo "Enter the name of the domain to be configured."
 echo "Use it without www, http or https:"
-echo -e "${BLUE}Please enter your domain name:" tput sgr0
+echo -e "${BLUE}Please enter your domain name:" ;tput sgr0
 read DOMAIN
 
 ### 2. Step - Create directory and adjust rights
@@ -46,17 +46,17 @@ systemctl reload apache2;
 
 ### 6. Step - Get SSL certificates
 clear;
-echo -e "${GREEN}It will create an SSL certificate for the following domain: $DOMAIN" tput sgr0
+echo -e "${GREEN}It will create an SSL certificate for the following domain: $DOMAIN" ;tput sgr0
 sleep 2
 sudo certbot --apache -d $DOMAIN -d www.$DOMAIN
 
 ### 7. Step - Create test page
-echo -e "${GREEN}A test page will be created for the following domain: https://$DOMAIN" tput sgr0
+echo -e "${GREEN}A test page will be created for the following domain: https://$DOMAIN" ;tput sgr0
 printf "
 <p>This is a simple test page</p>" | sudo tee /var/www/$DOMAIN/index.php
 
 ### 8. Step - Manual check
 clear;
-echo -e "${GREEN}Enter the following domain in the browser: $DOMAIN" tput sgr0
+echo -e "${GREEN}Enter the following domain in the browser: $DOMAIN" ;tput sgr0
 sleep 5;
 exit 0;
